4b913d2066b7e30480db4e2eb4c066a38ca64c97
Shahzad Amjad Khan <shahzad.amjad.khan@redmath.com>
2024-10-28 15:03:59 +0500

    Updated isobic to CSV

--- isobic/isobic_pdf_to_csv.py
+++ isobic/isobic_pdf_to_csv.py
@@ -9,6 +9,8 @@ import PyPDF2
 import requests
 import shutil
 
+
+
  
 URL = "https://www.iso9362.org/bicservice/public/v1/bicdata/_pdf"
 TEMP_FOLDER = "temp"
@@ -27,8 +29,18 @@ def create_directories():
 
 
 def download_pdf():
-    print("{}: Startiong download file: {}".format(datetime.datetime.now(), URL))
-    response = requests.get(URL,timeout=120)
+    print("{}: Starting download file: {}".format(datetime.datetime.now(), URL))
+    request_headers = {
+        "Accept-Language": "en-US,en;q=0.9",
+        "Cache-Control": "no-cache",
+        "Connection": "keep-alive",
+        "Cookie": "ISO9362_COOKIE=2349340844.47873.0000",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "none",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36"
+    }
+    response = requests.get(URL,headers=request_headers, timeout=120)
     if response.content:
         with open(PDF_FILE_PATH, 'wb') as f:
             f.write(response.content)
@@ -40,28 +52,28 @@ def download_pdf():
 
 def convert_pdf():
 
-    file = open(PDF_FILE_PATH, 'rb')
-    pdf = PyPDF2.PdfReader(file)
-    total_pages = len(pdf.pages)
-    print("Total PDF Pages: {}".format(total_pages))
+    with open(PDF_FILE_PATH, 'rb') as file:
+        pdf = PyPDF2.PdfReader(file)
+        total_pages = len(pdf.pages)
+        print("Total PDF Pages: {}".format(total_pages))
 
-    first_page = 1
-    last_page = READ_CHUNK
+        first_page = 1
+        last_page = READ_CHUNK
 
-    for i in range(first_page, total_pages, READ_CHUNK):
-        pages_to_read = str(i) + "-" + str(last_page)
-        print("{} Reading PDF: {} ".format(datetime.datetime.now(), pages_to_read))
-        csv_file_name = os.path.join(TEMP_FOLDER,"ISOBIC_" + str(i) + "-" + str(last_page) + ".csv")
-        col2str = {'dtype': str}
-        list_of_dfs = tabula.read_pdf(URL, pages=pages_to_read, lattice=True, format="CSV", pandas_options=col2str)
-        result_df = pd.concat(list_of_dfs)
-        result_df.to_csv(csv_file_name,quoting=csv.QUOTE_ALL)
-        print("{}: File created: {}".format(datetime.datetime.now(), csv_file_name))
-        last_page = last_page + READ_CHUNK
-        if last_page > total_pages:
-            last_page = total_pages
-
-    file.close()
+        for i in range(first_page, total_pages, READ_CHUNK):
+            pages_to_read = str(i) + "-" + str(last_page)
+            print("{} Reading PDF: {} ".format(datetime.datetime.now(), pages_to_read))
+            csv_file_name = os.path.join(TEMP_FOLDER,"ISOBIC_" + str(i) + "-" + str(last_page) + ".csv")
+            col2str = {'dtype': str}
+            list_of_dfs = tabula.read_pdf(file, pages=pages_to_read, lattice=True, format="CSV", pandas_options=col2str)
+            result_df = pd.concat(list_of_dfs)
+            result_df.columns = result_df.columns.str.replace(r"[\r\n\t]", " ", regex=True)
+            result_df.replace(to_replace=[r"\\t", r"\\n", r"\\r", "\t", "\n", "\r"], value=" ", regex=True, inplace=True)
+            result_df.to_csv(csv_file_name,quoting=csv.QUOTE_ALL, index=False)
+            print("{}: File created: {}".format(datetime.datetime.now(), csv_file_name))
+            last_page = last_page + READ_CHUNK
+            if last_page > total_pages:
+                last_page = total_pages
 
 
 def merge_csvs():
@@ -69,7 +81,7 @@ def merge_csvs():
     csv_files = glob.glob(TEMP_FOLDER + '/*.{}'.format('csv'))
 
     df_csv_append = pd.concat(map(pd.read_csv, csv_files), ignore_index=True)
-    df_csv_append.to_csv(os.path.join(OUTPUT_FOLDER, "ISOBIC.csv"), quoting=csv.QUOTE_ALL)
+    df_csv_append.to_csv(os.path.join(OUTPUT_FOLDER, "ISOBIC.csv"), quoting=csv.QUOTE_ALL, index=False)
     print ("{}: CSVs Merged".format(datetime.datetime.now()))
 
 
@@ -81,9 +93,9 @@ def remove_directories():
 if __name__ == "__main__":
     print("{}: Starting process to convert ISOBIC PDF from {} to CSV".format(datetime.datetime.now(), URL))
     warnings.filterwarnings("ignore")
-    create_directories()
-    download_pdf()
+    # create_directories()
+    # download_pdf()
     convert_pdf()
     merge_csvs()
-    remove_directories()
+    # remove_directories()
     print("{}: Process completed".format(datetime.datetime.now()))
